ecff2261e971c1288e45ba92103e84abde91fad5
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-05-25 16:03:40 +0200

    antivirus

--- src/bubble.ts
+++ src/bubble.ts
@@ -21,6 +21,10 @@ export function createBubble(
     return new VirusBubble(renderer, pos, radius, 99999, vel);
   }
 
+  if (Math.random() < cfg.ANTIVIRUS_BUBBLE_PROBABILITY) {
+    return new AntiVirusBubble(renderer, pos, 45, 3, vel);
+  }
+
   // A blackhole fills a lot of bubbles, increasing the spawn rate
   // thus making spawning a new blackhole more probable
   // so to avoid a vicious-cycle we half the probability for each already existing
@@ -46,6 +50,9 @@ type KillReason = {
 } | {
   type: "bubble",
   bubble: Bubble,
+} | {
+  type: "antivirus_ray",
+  antivirus: AntiVirusBubble,
 };
 
 export class Bubble implements Entity {
@@ -382,6 +389,8 @@ export class BlackholeBubble extends Bubble {
         continue;
       if (bubble instanceof BlackholeBubble)
         continue;
+      if (bubble instanceof AntiVirusBubble)
+        continue;
 
       const diff = this.pos.clone().sub(bubble.pos);
       const dist = diff.norm();
@@ -456,6 +465,8 @@ export class VirusBubble extends Bubble {
         continue;
       if (bubble instanceof BlackholeBubble)
         continue;
+      if (bubble instanceof AntiVirusBubble)
+        continue;
 
       const value = -bubble.velocity.norm2();
       if (value > targetValue) {
@@ -501,8 +512,147 @@ export class VirusBubble extends Bubble {
       }
       return;
     }
-    const canKill = [BlackholeBubble, VirusBubble];
+    const canKill = [BlackholeBubble, AntiVirusBubble];
     if (reason.type === "bubble" && !canKill.some(class_ => reason.bubble instanceof class_))
+      return;
+
+    super.kill(reason);
+  }
+}
+
+export class AntiVirusBubble extends Bubble {
+  private rays: { age: number, target: Vec2 }[] = [];
+  private lastRay: number;
+
+  constructor(
+    renderer: Renderer,
+    
+    pos: Vec2,
+    radius: number,
+    life: number,
+    velocity: Vec2,
+  ) {
+    super(renderer, pos, radius, life, velocity);
+
+    this.targetVelocity = Vec2.ZERO;
+    this.colorCfg = cfg.ANTIVIRUS_BUBBLE_COLOR;
+
+    this.lastRay = renderer.totalTime;
+  }
+
+  protected override forceMultiplierWith(_other: Bubble): number {
+    return 0;
+  }
+
+  protected override updateMouseCollision(): void { }
+
+  protected override get velocityInterpolationSpeed(): number {
+    return 50;
+  }
+
+  public override get zindex() {
+    return super.zindex + 5;
+  }
+
+  public override draw() {
+    const ctx = this.renderer.ctx;
+
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.radius / 5;
+    ctx.lineCap = "round";
+
+    const angles = [
+      Math.PI,
+      - Math.PI / 5,
+        Math.PI / 5,
+    ].map(v => v - Math.PI / 2);
+
+    ctx.beginPath();
+    for (const angle of angles) {
+      ctx.moveTo(this.pos.x, this.pos.y);
+      const to = this.pos.clone().add(Vec2.rotated(angle).mul(this.radius));
+      ctx.lineTo(to.x, to.y);
+    }
+    ctx.moveTo(this.pos.x + this.radius, this.pos.y);
+    ctx.arc(
+      this.pos.x, this.pos.y,
+      this.radius,
+      0, Math.PI * 2,
+    );
+    ctx.stroke();
+
+    if (this.isDying())
+      return;
+
+    for (const ray of this.rays) {
+      console.log(ray);
+      const animation_t = (ray.age / cfg.ANTIVIRUS_RAY_ANIMATION_DURATION);
+      ctx.strokeStyle = `rgba(255,255,255,${1-animation_t})`;
+      ctx.lineWidth = animation_t * 20 + 5;
+      ctx.lineCap = "round";
+
+      ctx.beginPath();
+      const diff = ray.target.clone().sub(this.pos);
+      const dir = diff.div(diff.norm());
+      const start = this.pos.clone().add(dir.mul(this.radius));
+      ctx.moveTo(start.x, start.y);
+      ctx.lineTo(ray.target.x, ray.target.y);
+      ctx.stroke();
+    }
+  }
+
+  private findTarget(): Bubble | null {
+    for (const bubble of this.renderer.bubbles) {
+      if (bubble === this)
+        continue;
+      if (bubble.isDying())
+        continue;
+      if (bubble instanceof VirusBubble)
+        return bubble;
+    }
+    return null;
+  }
+
+  public override update() {
+    super.update();
+    const dt = this.renderer.dt;
+
+    if (this.renderer.totalTime - this.lastRay > cfg.ANTIVIRUS_RAY_COOLDOWN) {
+      this.lastRay = this.renderer.totalTime;
+      const target = this.findTarget();
+      if (target) {
+        target.kill({type: "antivirus_ray", antivirus: this});
+        this.rays.push({
+          age: 0,
+          target: target.pos,
+        });
+      }
+    }
+
+    for (const ray of this.rays) {
+      ray.age += dt;
+    }
+    
+    this.rays = this.rays.filter(ray => ray.age < cfg.ANTIVIRUS_RAY_ANIMATION_DURATION);
+  }
+
+  public override kill(reason: KillReason) {
+    if (reason.type === "wall") {
+      if (reason.dir.eq(0, 1)) {
+        this.pos.y = this.renderer.canvas.height - this.radius;
+      }
+      if (reason.dir.eq(0, -1)) {
+        this.pos.y = this.radius;
+      }
+      if (reason.dir.eq(1, 0)) {
+        this.pos.x = this.renderer.canvas.width - this.radius;
+      }
+      if (reason.dir.eq(-1, 0)) {
+        this.pos.x = this.radius;
+      }
+      return;
+    }
+    if (reason.type === "bubble")
       return;
 
     super.kill(reason);
--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,13 @@ export const BACKGROUND_COLOR = "#202020";
 export const BUBBLE_DYING_DURATION = 0.5;
 export const MOUSE_CLICK_COOLDOWN = 2;
 
+export const ANTIVIRUS_RAY_COOLDOWN = 0.25;
+export const ANTIVIRUS_RAY_ANIMATION_DURATION = 1;
+
 export const GOLD_BUBBLE_PROBABILITY = 0.01;
 export const BLACKHOLE_BUBBLE_PROBABILITY = 0.001;
 export const VIRUS_BUBBLE_PROBABILITY = 0.005;
+export const ANTIVIRUS_BUBBLE_PROBABILITY = 0.005;
 
 export const DEFAULT_BUBBLE_COLOR: BubbleColorCfg = {
   default: Object.freeze([100, 136, 234] as const),
@@ -23,4 +27,8 @@ export const BLACKHOLE_BUBBLE_COLOR: BubbleColorCfg = {
 export const VIRUS_BUBBLE_COLOR: BubbleColorCfg = {
   default: Object.freeze([119, 221, 119] as const),
   close: Object.freeze([119, 221, 119] as const),
+};
+export const ANTIVIRUS_BUBBLE_COLOR: BubbleColorCfg = {
+  default: Object.freeze([255,255,255] as const),
+  close: Object.freeze([255,255,255] as const),
 };
