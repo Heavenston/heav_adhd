0281e9b6f6be4fe2595346baf4025d6283bb3987
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2024-05-29 02:18:00 +0200

    generic spawner

--- src/tool.ts
+++ src/tool.ts
@@ -2,7 +2,7 @@ import { ForceField } from "./force_field";
 import { Vec2 } from "./math";
 import { Renderer } from "./renderer";
 import * as cfg from "./config";
-import { createBubble } from "./bubble";
+import { bubbleClasses, createBubble } from "./bubble";
 
 type ToolClass = {
   new (renderer: Renderer): Tool,
@@ -15,8 +15,6 @@ export abstract class Tool {
   constructor(renderer: Renderer) {
     this.renderer = renderer;
     this.abortController = new AbortController();
-
-    this.init();
   }
 
   public static getFromName(name: string): ToolClass | null {
@@ -87,6 +85,44 @@ export class DeleterTool extends Tool {
 
 export class SpawnTool extends Tool {
   public lastPlace: number = -Infinity;
+  public cfgElement: HTMLDivElement;
+
+  constructor(renderer: Renderer) {
+    super(renderer);
+    const el = document.getElementById("toolConfig");
+    if (el === null || !(el instanceof HTMLDivElement))
+      throw new Error("Could not find config element");
+    this.cfgElement = el;
+  }
+
+  public override init() {
+    const options = Object.entries(bubbleClasses).map(([name, val]) => {
+      const displayName = val.class_.displayName.split(" ")
+        .map(d => d[0].toUpperCase() + d.slice(1))
+        .join(" ");
+      return `<option value="${name}">${displayName}</option>`;
+    });
+    this.cfgElement.innerHTML = `
+      <label>
+        <span>Spawn bubble type: </span>
+        <select id="spawner-tool-type">
+          <option value="random">Random</option>
+          ${options.join("")}
+        </select>
+      </label>
+    `;
+  }
+
+  public override clean() {
+    this.cfgElement.innerHTML = "";
+  }
+
+  private get spawnerTypeInput(): HTMLSelectElement | null {
+    const el = document.getElementById("spawner-tool-type");
+    if (!el || !(el instanceof HTMLSelectElement))
+      return null;
+    return el;
+  }
 
   public override update() {
     const rend = this.renderer;
@@ -100,7 +136,17 @@ export class SpawnTool extends Tool {
     if (!mp)
       return;
 
-    const bubble = createBubble(rend, { pos: mp, });
+    const kind = this.spawnerTypeInput?.value ?? "random";
+
+    let bubble;
+    if (kind === "random") {
+      bubble = createBubble(rend, { pos: mp });
+    }
+    else {
+      const i = bubbleClasses[<keyof typeof bubbleClasses>kind];
+      bubble = new i.class_(rend, { pos: mp });
+    }
+
     if (this.renderer.trySpawnBall(bubble)) {
       this.lastPlace = this.renderer.totalTime;
     }
